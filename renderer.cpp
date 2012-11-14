0bc9603ec22b3f9c0954ce277e6eda271f64fc0e
Nikunj Patel <np1509@gmail.com>
2012-11-14 19:41:38 +0000

    Removed DX9 render path

--- renderer.cpp
+++ renderer.cpp
@@ -1,27 +1,12 @@
 //--------------------------------------------------------------------------------------
-// File: EmptyProject11.cpp
+// File: main.cpp
 //
 // Empty starting point for new Direct3D 9 and/or Direct3D 11 applications
 //
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //--------------------------------------------------------------------------------------
+
 #include "DXUT.h"
-
-
-//--------------------------------------------------------------------------------------
-// Extern declarations 
-//--------------------------------------------------------------------------------------
-extern bool CALLBACK IsD3D9DeviceAcceptable( D3DCAPS9* pCaps, D3DFORMAT AdapterFormat, D3DFORMAT BackBufferFormat,
-                                             bool bWindowed, void* pUserContext );
-extern HRESULT CALLBACK OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice,
-                                            const D3DSURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext );
-extern HRESULT CALLBACK OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
-                                           void* pUserContext );
-extern void CALLBACK OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, float fElapsedTime,
-                                        void* pUserContext );
-extern void CALLBACK OnD3D9LostDevice( void* pUserContext );
-extern void CALLBACK OnD3D9DestroyDevice( void* pUserContext );
-
 
 //--------------------------------------------------------------------------------------
 // Reject any D3D11 devices that aren't acceptable by returning false
@@ -77,7 +62,7 @@ void CALLBACK OnD3D11FrameRender( ID3D11Device* pd3dDevice, ID3D11DeviceContext*
                                   double fTime, float fElapsedTime, void* pUserContext )
 {
     // Clear render target and the depth stencil 
-    float ClearColor[4] = { 0.176f, 0.196f, 0.667f, 0.0f };
+    float ClearColor[4] = { 1.0f, 0.196f, 0.667f, 0.0f };
 
     ID3D11RenderTargetView* pRTV = DXUTGetD3D11RenderTargetView();
     ID3D11DepthStencilView* pDSV = DXUTGetD3D11DepthStencilView();
@@ -149,9 +134,6 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
     _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 #endif
 
-    // DXUT will create and use the best device (either D3D9 or D3D11) 
-    // that is available on the system depending on which D3D callbacks are set below
-
     // Set general DXUT callbacks
     DXUTSetCallbackFrameMove( OnFrameMove );
     DXUTSetCallbackKeyboard( OnKeyboard );
@@ -159,14 +141,6 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
     DXUTSetCallbackMsgProc( MsgProc );
     DXUTSetCallbackDeviceChanging( ModifyDeviceSettings );
     DXUTSetCallbackDeviceRemoved( OnDeviceRemoved );
-
-    // Set the D3D9 DXUT callbacks. Remove these sets if the app doesn't need to support D3D9
-    DXUTSetCallbackD3D9DeviceAcceptable( IsD3D9DeviceAcceptable );
-    DXUTSetCallbackD3D9DeviceCreated( OnD3D9CreateDevice );
-    DXUTSetCallbackD3D9DeviceReset( OnD3D9ResetDevice );
-    DXUTSetCallbackD3D9FrameRender( OnD3D9FrameRender );
-    DXUTSetCallbackD3D9DeviceLost( OnD3D9LostDevice );
-    DXUTSetCallbackD3D9DeviceDestroyed( OnD3D9DestroyDevice );
 
     // Set the D3D11 DXUT callbacks. Remove these sets if the app doesn't need to support D3D11
     DXUTSetCallbackD3D11DeviceAcceptable( IsD3D11DeviceAcceptable );
@@ -180,11 +154,10 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
 
     DXUTInit( true, true, NULL ); // Parse the command line, show msgboxes on error, no extra command line params
     DXUTSetCursorSettings( true, true ); // Show the cursor and clip it when in full screen
-    DXUTCreateWindow( L"EmptyProject11" );
+    DXUTCreateWindow( L"Renderer" );
 
-    // Only require 10-level hardware
-    DXUTCreateDevice( D3D_FEATURE_LEVEL_10_0, true, 640, 480 );
-    DXUTMainLoop(); // Enter into the DXUT ren  der loop
+    DXUTCreateDevice( D3D_FEATURE_LEVEL_11_0, true, 640, 480 );
+    DXUTMainLoop(); // Enter into the DXUT render loop
 
     // Perform any application-level cleanup here
 
